6380522b828750545a795b98a0b2d10c626d5a91
giova-schade <gio.schade.c@gmail.com>
2024-11-09 20:57:00 -0300

    feature El top 10 histórico de usuarios (username) más influyentes

--- src/q3_memory.py
+++ src/q3_memory.py
@@ -1,5 +1,33 @@
 from typing import List, Tuple
 
+from sqlalchemy import text
 
-def q3_memory(file_path: str) -> List[Tuple[str, int]]:  # type: ignore
-    pass
+from utils.db_connection import get_session
+
+
+def q3_memory() -> List[Tuple[str, int]]:
+    """
+    Optimiza el uso de memoria al delegar el conteo y agrupación de menciones al motor de la base de datos
+    """
+    session = get_session()
+    query = text(
+        """
+        SELECT mention, COUNT(*) AS mention_count
+        FROM (
+            SELECT jsonb_array_elements_text(m.mentions::jsonb) AS mention
+            FROM latam.mentions m
+        ) subquery
+        GROUP BY mention
+        ORDER BY mention_count DESC
+        LIMIT 10;
+        """
+    )
+
+    result = session.execute(query).fetchall()
+    session.close()
+
+    top_mentions: List[Tuple[str, int]] = [
+        (row.mention, row.mention_count) for row in result
+    ]
+
+    return top_mentions
--- src/q3_time.py
+++ src/q3_time.py
@@ -1,5 +1,38 @@
+import json
+from collections import Counter
 from typing import List, Tuple
 
+from sqlalchemy import text
 
-def q3_time(file_path: str) -> List[Tuple[str, int]]:  # type: ignore
-    pass
+from utils.db_connection import get_session
+
+
+def q3_time() -> List[Tuple[str, int]]:
+    """
+    Recupera y procesa los datos de la base de datos para obtener el top 10 de usuarios más influyentes
+    en función del conteo de las menciones.
+    """
+    session = get_session()
+    query = text(
+        """
+        SELECT m.mentions
+        FROM latam.mentions m;
+        """
+    )
+
+    result = session.execute(query).fetchall()
+    session.close()
+
+    mention_counter: Counter[str] = Counter()
+
+    for row in result:
+        mentions = row.mentions
+        if isinstance(mentions, str):
+            mentions = json.loads(mentions)
+
+        if isinstance(mentions, list):
+            mention_counter.update(mentions)
+
+    top_mentions: List[Tuple[str, int]] = mention_counter.most_common(10)
+
+    return top_mentions
